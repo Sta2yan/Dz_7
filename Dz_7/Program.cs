067b035a5c648f62670f9048f3aa9dd40644313b
Sta2yan <s2_stas@mail.ru>
2022-02-26 22:39:33 +0300

    Добавьте файлы проекта.

---
+++ Dz_7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dz_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите текст для вывода: ");
+            string userText = Console.ReadLine();
+            Console.Write("Введите сколько раз нужно вывести текст: ");
+            int countTextOutput = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < countTextOutput; i++) Console.WriteLine((i + 1) + ". " + userText);
+        }
+    }
+}
